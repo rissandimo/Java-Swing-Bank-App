7d0e20e17074c43937a2cd2ff43159d6bf3ce5ce
Omid <nassiromid@gmail.com>
2017-02-27 20:37:12 -0800

    Added empty JFrame

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-/**
- * Created by Omid on 2/27/2017.
- */
-public class Main {
+import javax.swing.*;
+
+public class Main
+{
+    private JFrame frame;
+
+    private Main()
+    {
+        createFrame();
+    }
+
+    private void createFrame()
+    {
+        frame = new JFrame("Bank");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(450, 350);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        new Main();
+    }
 }
