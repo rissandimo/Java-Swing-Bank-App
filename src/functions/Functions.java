6da1e7ecfeefeac307b42d89881d161aa2ad6bf1
rissandimo <nassiromid@gmail.com>
2017-05-23 19:29:48 -0700

    Fixed issue checking for validating first name and last name

--- src/functions/Functions.java
+++ src/functions/Functions.java
@@ -5,21 +5,23 @@ public class Functions
     // will return true if string is empty or null - returning false means its good
     public static boolean isStringEmpty(String string)
     {
-        boolean empty = true;
+        boolean empty;
         String emptyString; // removed newString() - because compiler stated that it's reduntant
         String nullString = null;
+        System.out.println("String length: " + string.length());
 
-        // if string contains white spaces
-        if(string.trim().length() >= 0) {
+        if(string.length() == 0) { // empty string
          empty = true;
-            System.out.println("String contains white spaces");
         }
-            // if string is not null
-            if(!string.equals(nullString))
-            {
-                empty = false;
-                System.out.println("String is null");
-            }
+        else if(string.trim().length() == 0) // checks for white spaces
+        {
+            System.out.println("0 after trim length");
+            empty = true;
+        }
+        else
+        {
+            empty = false;
+        }
         return empty;
     }
 }
