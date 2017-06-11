334df38998c3e6413cef811024eefe38008b71c4
Omid <nassiromid@gmail.com>
2017-06-11 10:05:24 -0700

    Added method checkClientInfo - Runs isStringEmpty method Added method isEmailCorrect

--- src/functions/Functions.java
+++ src/functions/Functions.java
@@ -1,21 +1,41 @@
 package functions;
 
+import javax.swing.*;
+
 public class Functions
 {
+
+    public static boolean checkClientInfo(String clientInfo)
+    {
+        boolean informationCorrect;
+
+        if(isStringEmpty(clientInfo))
+        {
+            informationCorrect = false;
+        }
+        else
+        {
+            informationCorrect = true;
+        }
+
+        return  informationCorrect;
+    }
+
+
     // will return true if string is empty or null - returning false means its good
     public static boolean isStringEmpty(String string)
     {
         boolean empty;
         String emptyString; // removed newString() - because compiler stated that it's reduntant
         String nullString = null;
-        System.out.println("String length: " + string.length());
 
-        if(string.length() == 0) { // empty string
+
+        if(string.length() == 0)
+        { // empty string
          empty = true;
         }
         else if(string.trim().length() == 0) // checks for white spaces
         {
-            System.out.println("0 after trim length");
             empty = true;
         }
         else
@@ -23,5 +43,26 @@ public class Functions
             empty = false;
         }
         return empty;
+    }
+
+    public static boolean isEmailCorrect(String email)
+    {
+        int numOfSigns = 0;
+        boolean correct = false;
+
+        for(int i=0; i < email.length(); i++)
+        {
+            if(email.charAt(i) == '@')
+            {
+                numOfSigns++;
+            }
+        }
+
+        if(numOfSigns == 1)
+        {
+          correct = true;
+        }
+
+        return correct;
     }
 }
