0990e4676c87d5856d9870e76230eb5721af03bb
omid nassir <nassiromid@gmail.com>
2017-06-20 16:18:02 -0700

    Created and added textAreas and buttons

--- src/Account.java
+++ src/Account.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
 
- class Account
+class Account
 {
    private String email, firstName, lastName, social;
     Account(String clientFirstName, String clientLastName, String clientEmail, String clientSocial)
@@ -13,11 +14,95 @@ import javax.swing.*;
         startProgram();
     }
 
+    public Account()
+    {
+        startProgram();
+    }
+
     private void startProgram()
     {
+        createFrame();
+    }
+
+    private void createFrame()
+    {
+
         JFrame frame = new JFrame(firstName + "'s account");
-        frame.setSize(350, 250);
+
+        TextAreaPanel textAreaPanel = new TextAreaPanel();
+        SelectionPanel selectionPanel = new SelectionPanel();
+        BottomPanel bottomPanel = new BottomPanel();
+
+        frame.add(selectionPanel, BorderLayout.NORTH);
+        frame.add(textAreaPanel, BorderLayout.CENTER);
+        frame.add(bottomPanel, BorderLayout.SOUTH);
+
+        frame.setBounds(300, 300, 400, 400);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
-}
+
+    class TextAreaPanel extends JPanel
+    {
+        private JTextArea textArea_Result;
+
+         TextAreaPanel()
+        {
+            textArea_Result = new JTextArea(15,30);
+            add(textArea_Result);
+        }
+    }// TextAreaPanel
+
+    class SelectionPanel extends JPanel
+    {
+
+         SelectionPanel()
+        {
+
+            JLabel label_makeSelection = new JLabel("Please make a selection:");
+            add(label_makeSelection);
+
+            ButtonsPanel buttonsPanel = new ButtonsPanel();
+            add(buttonsPanel);
+        }
+
+        class ButtonsPanel extends JPanel
+        {
+            ButtonsPanel()
+            {
+
+            JButton button_deposit, button_withdrawal, button_checkAcctInfo;
+
+            button_deposit = new JButton("Deposit");
+            button_withdrawal = new JButton("Withdrawal");
+            button_checkAcctInfo = new JButton("Account Info");
+
+            add(button_deposit);
+            add(button_withdrawal);
+            add(button_checkAcctInfo);
+            }
+
+        }
+    }
+
+    class BottomPanel extends JPanel
+    {
+        JButton button_logOut, button_submit;
+
+         BottomPanel()
+        {
+            button_logOut = new JButton("Log Out");
+            button_submit = new JButton("Submit");
+
+            add(button_logOut);
+            add(button_submit);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        new Account();
+    }
+
+
+}// Account
