9d962b41dd004575392baefc7665063e3022f3c7
Omid <nassiromid@gmail.com>
2017-08-27 15:18:31 -0700

    Initial commit Original code from Main, moved into a new Class called WelcomeScreen Button access account connects to a Bank database, replaced it with Lambada Button create account is not registered with a listener

---
+++ src/view/WelcomeScreen.java
@@ -0,0 +1,61 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Allows customer to access existing account or to create a new one
+ */
+public class WelcomeScreen extends JFrame
+{
+
+        private JButton buttonCreateAccount;
+        private JButton buttonAccessAccount;
+        private JLabel labelSelection;
+        private JPanel buttonsPanel;
+        private JPanel labelPanel;
+
+    public WelcomeScreen() {
+        System.out.println("Welcome screen");
+
+
+        setBounds(400, 300, 500, 400);
+        setTitle("Nassir Bank");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        //BUTTONS
+
+        buttonAccessAccount = new JButton("Access Account");
+        buttonAccessAccount.addActionListener(e ->
+        {
+            dispose();
+            new OpenAccount();
+        });
+
+        buttonCreateAccount = new JButton("Create Account");
+
+        //LABELS
+
+        labelSelection = new JLabel("Please make a selection below");
+
+
+        //PANELS
+        buttonsPanel = new JPanel();
+        labelPanel = new JPanel();
+
+
+        // ADD COMPONENTS TO FRAME
+
+        buttonsPanel.add(buttonAccessAccount);
+        buttonsPanel.add(buttonCreateAccount);
+
+        labelPanel.add(labelSelection);
+
+        add(buttonsPanel, BorderLayout.SOUTH);
+        add(labelPanel, BorderLayout.CENTER);
+
+        setVisible(true);
+        }
+    }
