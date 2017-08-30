b96b12cd33da8f7f206a2b94033ea202503046b8
Omid <nassiromid@gmail.com>
2017-08-29 21:24:12 -0700

    Removed old imports

--- src/view/WelcomeScreen.java
+++ src/view/WelcomeScreen.java
@@ -2,8 +2,6 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
  * Allows customer to access existing account or to create a new one
