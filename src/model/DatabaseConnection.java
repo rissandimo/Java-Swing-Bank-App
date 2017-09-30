767b0f4898305b51ceb3d91e00e5903e2337d421
Omid <nassiromid@gmail.com>
2017-09-30 00:07:16 -0700

    Cleaned up code

--- src/model/DatabaseConnection.java
+++ src/model/DatabaseConnection.java
@@ -21,14 +21,6 @@ public class DatabaseConnection
     {
     }
 
-/*    public DatabaseConnection(String first_name, String last_name, String ssn)
-    {
-        DatabaseConnection.first_name = first_name;
-        DatabaseConnection.last_name = last_name;
-        DatabaseConnection.ssn = ssn;
-
-        createConnectionToDatabase();
-    }*/
 
     public Connection createConnectionToDatabase()
     {
@@ -39,22 +31,6 @@ public class DatabaseConnection
             connection = DriverManager.getConnection("jdbc:mysql://localhost/bank?autoReconnect=true&useSSL=false", "root", "password");
 
 
-/*
-            String sqlQuery = "SELECT * FROM clients";
-
-            Statement statement = connection.createStatement();
-
-
-            ResultSet resultSet = statement.executeQuery(sqlQuery);
-
-            System.out.println("Database Accessed");
-
-            System.out.println("Client info: ");
-
-            while (resultSet.next())
-            {
-                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
-            }*/
 
         }
         catch (SQLException s)
