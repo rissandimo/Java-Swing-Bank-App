80ed441710886d65a9ebe23ef1e9dcc65a8e1baa
Omid - Acer <nassiromid@gmail.com>
2017-12-09 23:58:58 -0800

    changed addClientInfo method: the account# starts at 1,000,000 but doesn't increment
    the client and checking_account tables now have constraints

--- src/controller/CreateClientAccount.java
+++ src/controller/CreateClientAccount.java
@@ -12,102 +12,62 @@ import java.sql.*;
 
 public class CreateClientAccount
 {
+
+    //TESTING PURPOSES
+
+    /*    public static void main(String[] args)
+        {
+            new CreateClientAccount();
+        }*/
     private DatabaseConnection bankConnection;
 
     private static final int ACCOUNT_NUMBER = 1000000;
 
-    private String firstName, lastName, socialSecurityNumber;
+    private String firstName, lastName, social;
 
-     private int accountNumbers[] = new int[10];
+    //  private int accountNumbers[] = new int[10];
 
-    public CreateClientAccount(String firstName, String lastName, String socialSecurityNumber)
+    public CreateClientAccount(String firstName, String lastName, String social)
     {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.socialSecurityNumber = socialSecurityNumber;
+        this.social = social;
 
-       bankConnection = new DatabaseConnection();
+        bankConnection = new DatabaseConnection();
 
-       addClientToDatabase(firstName, lastName, socialSecurityNumber);
+        addClientInfo(firstName, lastName, social);
     }
 
-    private void addClientToDatabase(String firstName,String lastName,String ssn)
+    private void addClientInfo(String firstName, String lastName, String social)
     {
-        int accountNumberIndex = 0;
+        System.out.println("Add client info");
 
-        String createClientStatement = "INSERT INTO clients (first_name, last_name, ssn) values (?, ?, ?)";
-        String createCheckingStatement = "INSERT INTO checking_account(balance, account_number) values (?, ?)";
-
-        Connection sqlConnection = bankConnection.createConnectionToDatabase();
-
-       // System.out.println("Connection to database successful");
-
-
-        // add client to database
         try
         {
+            Connection sqlConnection = bankConnection.createConnectionToDatabase();
 
-        PreparedStatement preparedStatementClient = sqlConnection.prepareStatement(createClientStatement);
+            String createClientStatement = "INSERT INTO clients (first_name, last_name, social, account_number) values (?, ?, ?, ?)";
 
-        preparedStatementClient.setString(1, firstName);
-        preparedStatementClient.setString(2, lastName);
-        preparedStatementClient.setString(3, ssn);
+            try (PreparedStatement preparedStatementClient = sqlConnection.prepareStatement(createClientStatement))
+            {
+                preparedStatementClient.setString(1, firstName);
+                preparedStatementClient.setString(2, lastName);
+                preparedStatementClient.setString(3, social);
+                preparedStatementClient.setInt(4, ACCOUNT_NUMBER);
 
-        //insert client into database
-        boolean clientCreated = preparedStatementClient.execute();
+                System.out.println("client sql statement executing");
+                boolean clientCreated = preparedStatementClient.execute();
 
-        // add client info to checking account
+                //preparedStatementClient closes here
 
-            PreparedStatement preparedStatementAccount = sqlConnection.prepareStatement(createCheckingStatement);
-
-            preparedStatementAccount.setDouble(1, 0.0);
-            preparedStatementAccount.setInt(2, ACCOUNT_NUMBER);
-
-            boolean accountCreated = preparedStatementAccount.execute();
-
-
-            //print out to console if connection successfull
-
-        if(clientCreated) System.out.println("Client created successfully");
-        if(accountCreated) System.out.println("Account created successfully");
-
-
-        //search acct #s based on a ssn - testing purposes
-         String accountNumberSqlStatement = "SELECT account_number FROM clients WHERE ssn = ?";
-
-         preparedStatementClient = sqlConnection.prepareStatement(accountNumberSqlStatement);
-
-         preparedStatementClient.setString(1, socialSecurityNumber);
-
-
-         ResultSet accountNumberResultSet = preparedStatementClient.executeQuery();
-
-         //cycle through the existing account numbers
-         while(accountNumberResultSet.next())
-         {
-             accountNumbers[accountNumberIndex] = accountNumberResultSet.getInt(1);
-             accountNumberIndex++;
-         }
-
-         //print out the accoumt numbers
-        System.out.println("Account number: " + ACCOUNT_NUMBER);
-
-         //access ATM
-        new AccessAccount(ACCOUNT_NUMBER);
-
-        }
-        catch(SQLException e2)
-        {
-            e2.printStackTrace();
-        }
-        finally
-        {
-            try {
-                sqlConnection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
+                if (clientCreated) System.out.println("Client created successfully");
             }
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
         }
-    }
+    }// end add client
+
 
 }
