98649d4bf6992c50ca1a398d25f3ab0c4ebddfed
Omid - Acer <nassiromid@gmail.com>
2018-01-15 23:03:48 -0800

    Refactored code

--- src/controller/RemoveClientAccount.java
+++ src/controller/RemoveClientAccount.java
@@ -4,6 +4,8 @@ import model.DatabaseConnection;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.*;
 
 public class RemoveClientAccount
@@ -11,26 +13,83 @@ public class RemoveClientAccount
     private DatabaseConnection bankConnection;
     private static int ACCOUNT_NUMBER;
 
+
     public static void main(String[] args)
     {
-        new RemoveClientAccount().removeClient(5);
+        new RemoveClientAccount();
     }
 
     public RemoveClientAccount()
     {
         bankConnection = new DatabaseConnection();
-        //new Frame();
+        new Frame();
+    }
+
+
+
+    private void removeClient(int ACCOUNT_NUMBER)
+    {
+        Connection connection = bankConnection.createConnectionToDatabase();
+
+        String removeClientCK_WITHDRAWAL = "DELETE FROM checking_withdrawl where account_number = ?";
+        String removeClientCK_DEPOSIT = "DELETE FROM checking_deposit where account_number = ?";
+        String removeClientCK_ACCT = "DELETE FROM checking_account where account_number = ?";
+        String removeClient = "DELETE FROM clients where account_number = ?";
+
+        try
+        {
+                            //CHECKING WITHDRAWAL
+        PreparedStatement preparedStatement = connection.prepareStatement(removeClientCK_WITHDRAWAL);
+
+        preparedStatement.setInt(1, ACCOUNT_NUMBER);
+
+        preparedStatement.execute();
+
+                            //CHECKING DEPOSIT
+        preparedStatement = connection.prepareStatement(removeClientCK_DEPOSIT);
+
+        preparedStatement.setInt(1, ACCOUNT_NUMBER);
+
+        preparedStatement.execute();
+
+        System.out.println("client removed from checking_deposit");
+
+                            //CHECKING ACCOUNT
+        preparedStatement = connection.prepareStatement(removeClientCK_ACCT);
+
+        preparedStatement.setInt(1, ACCOUNT_NUMBER);
+
+        preparedStatement.execute();
+
+        System.out.println("client removed from checking_account");
+
+                            //CLIENT
+        preparedStatement = connection.prepareStatement(removeClient);
+
+        preparedStatement.setInt(1, ACCOUNT_NUMBER);
+
+        preparedStatement.execute();
+
+        System.out.println("client removed from clients");
+
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+
     }
 
     class Frame extends JFrame
     {
-        private JTextField input;
+        public JTextField input;
         private JTextArea results;
 
         public Frame()
         {
-           createView();
-           displayClients();
+            createView();
+            displayClients();
         }
 
         public void createView()
@@ -48,6 +107,7 @@ public class RemoveClientAccount
             //BOTTOM PANEL - Submit Buttons & Text Input
             input = new JTextField(15);
             JButton submit = new JButton("Submit");
+            submit.addActionListener(new RemoveClientListener());
 
 
             JPanel panelBottom = new JPanel();
@@ -80,9 +140,9 @@ public class RemoveClientAccount
                 while(resultSet.next())
                 {
                     results.append(resultSet.getString(1) + ", " +
-                                   resultSet.getString(2) + "-" +
-                                    "Account #: " +
-                                   resultSet.getString(3) + "\n");
+                            resultSet.getString(2) + "-" +
+                            "Account #: " +
+                            resultSet.getString(3) + "\n");
                 }
 
             }
@@ -91,52 +151,14 @@ public class RemoveClientAccount
                 e.printStackTrace();
             }
         }
-    }
 
-
-
-    private void removeClient(int ACCOUNT_NUMBER)
-    {
-        Connection connection = bankConnection.createConnectionToDatabase();
-
-        String removeClientCK_DEPOSIT = "DELETE FROM checking_deposit where account_number = ?";
-        String removeClientCK_ACCT = "DELETE FROM checking_account where account_number = ?";
-        String removeClient = "DELETE FROM clients where account_number = ?";
-
-        try
+        class RemoveClientListener implements ActionListener
         {
-        PreparedStatement preparedStatement = connection.prepareStatement(removeClientCK_DEPOSIT);
-
-        preparedStatement.setInt(1, ACCOUNT_NUMBER);
-
-        preparedStatement.execute();
-
-        System.out.println("client removed from checking_deposit");
-
-
-        preparedStatement = connection.prepareStatement(removeClientCK_ACCT);
-
-        preparedStatement.setInt(1, ACCOUNT_NUMBER);
-
-        preparedStatement.execute();
-
-        System.out.println("client removed from checking_account");
-
-
-        preparedStatement = connection.prepareStatement(removeClient);
-
-        preparedStatement.setInt(1, ACCOUNT_NUMBER);
-
-        preparedStatement.execute();
-
-        System.out.println("client removed from clients");
-
+            public void actionPerformed(ActionEvent e)
+            {
+                removeClient(Integer.parseInt(input.getText()));
+            }
         }
-        catch(SQLException e)
-        {
-            e.printStackTrace();
-        }
-    }
+    }// close Frame
 
-
-}
+} // close RemoveClientAccount
